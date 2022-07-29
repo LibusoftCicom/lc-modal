844dfde6d68e3c284ba562bb515adcdac0d75451
piivan <ivan.pintar@spi.hr>
2022-07-29 11:19:47 +0200

    preventScroll on modal close

--- projects/lc-modal-lib/src/lib/modal-factory.class.ts
+++ projects/lc-modal-lib/src/lib/modal-factory.class.ts
@@ -558,7 +558,7 @@ export class ModalFactory implements IModal<ModalFactory> {
 				if ((<any>this._focusElement).setActive) {
 					(<any>this._focusElement).setActive();
 				} else {
-					this._focusElement.focus();
+					this._focusElement.focus({ preventScroll: true });
 				}
 			} catch (err) {}
 		}
